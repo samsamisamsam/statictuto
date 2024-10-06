3c2a59c5daa1676a69f719f058a523a32b22c22f
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-10-06 16:07:32 -0400

    leafnode rendered to html + tested

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,11 +11,11 @@ class HTMLNode():
     def props_to_html(self):
         if self.props == {}:
             return ""
-        html_tags = ""
+        html_tags = " "
         for key, value in self.props.items():
             html_tags += f'{key}="{value}" '
         html_tags = html_tags.rstrip(' ')
-        return ' ' + html_tags
+        return html_tags
     
     def __repr__(self):
         return f"tag = {self.tag}, value = {self.value}, children = {self.children}, props = {self.props}"
